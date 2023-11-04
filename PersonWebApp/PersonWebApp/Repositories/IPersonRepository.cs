1cf8d2695578c2b0bfad01568d9c70d5291cf3de
Pocut Viqarunnisa <viqarunnisa@gmail.com>
2023-11-05 11:19:37 +1300

    personWebApp

---
+++ PersonWebApp/PersonWebApp/Repositories/IPersonRepository.cs
@@ -0,0 +1,10 @@
+﻿using PersonWebApp.Models;
+
+namespace PersonWebApp.Repositories
+{
+    public interface IPersonRepository
+    {
+        void AddPerson(Person person);
+        IEnumerable<Person> GetPeople();
+    }
+}
